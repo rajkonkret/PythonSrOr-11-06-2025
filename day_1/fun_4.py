494de9c2d1701189f2435bab41c238346aa4e700
rajkonkret <rajkonkret660@gmail.com>
2025-06-11 15:35:27 +0200

    [1 8] update fun3

---
+++ day_1/fun_4.py
@@ -0,0 +1,33 @@
+# funkcja rekurencyjna - wywołuje samą siebie
+
+def silnia(n):
+    if n == 0:
+        return 1
+    else:
+        return n * silnia(n - 1)
+
+
+print(silnia(5))
+
+
+def nwd(a, b):
+    if b == 0:
+        return a
+    return nwd(b, a % b)
+
+
+print(nwd(48, 18))  # 6
+
+
+def czy_pierwsza(n, dzielnik=2):
+    if n < 2:
+        return False
+    if dzielnik * dzielnik > n:
+        return True
+    if n % dzielnik == 0:
+        return False
+    return czy_pierwsza(n, dzielnik + 1)
+
+
+print(czy_pierwsza(29))  # True
+print(czy_pierwsza(30))  # False
