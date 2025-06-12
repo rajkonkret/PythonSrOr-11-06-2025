02037db805375d2fdea0c63735d808002846ad32
rajkonkret <rajkonkret660@gmail.com>
2025-06-12 12:37:49 +0200

    [2 11] update fun12.py add test

--- day_2/fun12.py
+++ day_2/fun12.py
@@ -9,3 +9,41 @@ transactions = [
     {"id": 6, "type": "expense", "amount": 400, "currency": "EUR"},
     {"id": 7, "type": "income", "amount": 100, "currency": "EUR"},
 ]
+
+
+def filter_transactions(transactions, transaction_type):
+    return list(filter(lambda x: x['type'] == transaction_type, transactions))
+
+
+def map_transactions(transactions, currency):
+    return list(map(lambda x: x['amount'] if x['currency'] == currency else 0, transactions))
+
+
+def reduce_transactions(transactions):
+    return reduce(lambda x, y: x + y, transactions, 0)
+
+# pip install pytest
+def process_transactions(transactions, transaction_type, currency):
+    filtered = filter_transactions(transactions, transaction_type)
+    mapped = map_transactions(filtered, currency)
+    total = reduce_transactions(mapped)
+    return total
+
+
+print(process_transactions(transactions, "expense", "EUR"))  # 400
+print(process_transactions(transactions, "income", "USD"))  # 2200
+
+
+def test_transaction_processing():
+    assert map_transactions(filter_transactions(transactions, "income"), "USD") == [1000, 500, 700, 0]
+
+# (.venv) PS C:\Users\CSComarch\PycharmProjects\PythonSrOr-11-06-2025\day_2> pytest .\fun12.py
+# ==================================================================================================== test session starts =====================================================================================================
+# platform win32 -- Python 3.13.4, pytest-8.4.0, pluggy-1.6.0
+# rootdir: C:\Users\CSComarch\PycharmProjects\PythonSrOr-11-06-2025\day_2
+# collected 1 item
+#
+# fun12.py .                                                                                                                                                                                                              [100%]
+#
+# ===================================================================================================== 1 passed in 0.01s ======================================================================================================
+# (.venv) PS C:\Users\CSComarch\PycharmProjects\PythonSrOr-11-06-2025\day_2>
