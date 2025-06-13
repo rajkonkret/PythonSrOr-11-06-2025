4b2f9a1c84fceb167cea4b047e31a6a477eb95dc
rajkonkret <rajkonkret660@gmail.com>
2025-06-13 15:30:10 +0200

    [3 14] add okno1.py

---
+++ day_3/okno1.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Pole tekstowe")
+root.geometry("300x150")
+
+label = tk.Label(root, text="Tekst pojawi się tutaj")
+label.pack(pady=10)
+
+entry = tk.Entry(root)
+entry.pack()
+
+
+def show_text(event=None):
+    text = entry.get()
+    label.config(text=text)
+
+
+button = tk.Button(root, text="Wyświetl tekst", command=show_text)
+button.pack(pady=5)
+
+entry.bind("<Return>", show_text)
+
+root.mainloop()
